81579dcd9df0fbf85064d4d7634cb94850581fe8
vlad663 <75929829+vlad663@users.noreply.github.com>
2021-03-14 16:50:14 +0700

    сделал новую рыбку и ее движение

--- pril.py
+++ pril.py
@@ -2,7 +2,7 @@ from PyQt5 import QtWidgets,QtCore
 from PyQt5.Qt import*
 from okno import Ui_Form
 from class_fish import Fish
-import time
+
 
 
 class MyWindow(QtWidgets.QWidget,Ui_Form):
@@ -15,40 +15,50 @@ class MyWindow(QtWidgets.QWidget,Ui_Form):
         self.obj=obj
         self.timer.timeout.connect(self.kik)
         self.timer.start()
-   
-    def table_move(self,obj):    
-        print('xaxa')
-        self.i+=1
-        self.b=str(self.i)
-        self.pushButton.setText("Ход"+" "+ self.b) 
-        self.c=QtWidgets.QTableWidgetItem ('1')
-        """тут правил """
+        self.all_fish=[]
         
-        self.tableWidget.setItem(self.obj.position[1],self.obj.position[0],self.c)    
-        
-    def clear_old_position(self,obj):
-        
-        self.c1=QtWidgets.QTableWidgetItem (None)
-        self.tableWidget.setItem(obj.old_position[1],obj.old_position[0],self.c1)
     def kik(self):
         """ Удаляем очищаем старую позицию """
         if self.obj.old_position[1]!=None:
             self.c1=QtWidgets.QTableWidgetItem (None)
             self.tableWidget.setItem(self.obj.old_position[1],self.obj.old_position[0],self.c1)
         
-
-        """ Создаем новую и отмечаем новый ход """
+        
+        """ Создаем новую  позицию и отмечаем новый ход """
         self.z=str(self.obj)
         #print(self.z)
         self.i+=1
         self.b=str(self.i)
-        self.pushButton.setText("Ход"+" "+ self.b) 
+        self.pushButton.setText("Ход"+" "+ self.b)  
         self.c=QtWidgets.QTableWidgetItem ('*')
         self.c.setTextAlignment(QtCore.Qt.AlignCenter)
         self.tableWidget.setItem(self.obj.position[1],self.obj.position[0],self.c)  
         self.obj.move()
+
+    
+        """ Добавляем новую рыбку"""
+        if self.pushButton.text() == 'Ход 7':
+            print('tut')
+            self.fish=Fish()
+            print(self.fish)
+            self.all_fish.append(self.fish)
+        """ Стираем старую позицию новой рыбки"""
+        for self.v in self.all_fish:
+            if self.v.old_position[1] !=None:
+                self.c1=QtWidgets.QTableWidgetItem (None)
+                self.tableWidget.setItem(self.v.old_position[1],self.v.old_position[0],self.c1)
+                
+        """ Движение новой рыбки"""
+        if self.all_fish!=[]:
+            for self.v  in self.all_fish:
+                self.c=QtWidgets.QTableWidgetItem ('*')
+                self.c.setTextAlignment(QtCore.Qt.AlignCenter)
+                self.tableWidget.setItem(self.v.position[1],self.v.position[0],self.c)  
+                self.v.move()
         
-        
+
+
+       
 def new_life_fish():
         x=Fish()
         return(x)
@@ -62,9 +72,23 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     
     all_fish.append(new_life_fish())
-    all_fish.append(new_life_fish())
     window = MyWindow(all_fish[0])
-    window = MyWindow(all_fish[1])
     window.show()                
     sys.exit(app.exec_())
     
+'''
+    def table_move(self,obj):    
+
+        self.Z=QtWidgets.QTableWidgetItem ('1')
+        """тут правил """
+        
+        self.tableWidget.setItem(self.obj.position[1],self.obj.position[0],self.Z)    
+        
+    def clear_old_position(self,obj):
+        
+        self.c1=QtWidgets.QTableWidgetItem (None)
+        self.tableWidget.setItem(obj.old_position[1],obj.old_position[0],self.c1)
+
+
+
+    '''
